fb48db1abb58a362d3ed2b7091b563aa268eb7bd
Samirgorai <samirgorai17@gmail.com>
2023-12-26 16:00:03 +0530

    Weather completed BMI completed
    Signed-off-by: Samir Gorai <samirgorai17@gmail.com>

--- projtemplate/basicapp/forms.py
+++ projtemplate/basicapp/forms.py
@@ -5,8 +5,17 @@ class Currency_form(forms.Form):
     #list containing conversion currency names 
     currency_list=[('INR','INR'),('USD','USD'),('EUR','EUR'),('GBP','GBP')]
 
-    base_currency=forms.ChoiceField(choices=currency_list,label='Base Currency')
-    amount=forms.FloatField(label='amount')
+    base_currency=forms.ChoiceField(choices=currency_list,label='Base Currency',)
     convert_currency=forms.ChoiceField(choices=currency_list,label='Convert Currency')
+    amount=forms.FloatField(label='amount')
 
 
+class Bmi_form(forms.Form):
+    weight=forms.FloatField(label='wight Kilogram')
+    height=forms.FloatField(label='height in cm')
+
+
+class Weather_form(forms.Form):
+    city=forms.CharField()
+    state=forms.CharField()
+    country=forms.CharField()
--- projtemplate/basicapp/views.py
+++ projtemplate/basicapp/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
-from basicapp.forms import Currency_form
+from basicapp.forms import Currency_form,Bmi_form,Weather_form
 import requests
 import json
 # Create your views here.
 
+#--------------------home page--------------------
+
 def index(request):
     return render(request,'basicapp/index.html')
 
+#-----------------------BMI---------------------
+
 def bmi(request):
-    
-    return render(request,'basicapp/BMIcalculator.html')
+    flag=False
+    calculated_BMI=0
+    category=''
+    if(request.method=='GET'):
+        bmi_form=Bmi_form(request.GET)
+        
+        if(bmi_form.is_valid()):
+            weight=float(request.GET.get('weight'))
+            height=float(request.GET.get('height'))/100
+            flag=True
+            calculated_BMI=weight/(height*height)
+            if(calculated_BMI<18.5):
+                category='Underweight:'
+            elif(calculated_BMI>=18.5 and calculated_BMI<24.9):
+                category='Normal weight:'
+            elif(calculated_BMI>=25 and calculated_BMI<29.9):
+                category='Overweight:'
+
+            else:
+                category='Obesity:'
+
+        else:
+            print(bmi_form.errors)
+
+    else:
+        bmi_form=Bmi_form()
+
+    return render(request,'basicapp/BMIcalculator.html',{'BMI_form':bmi_form,'calculated_BMI':calculated_BMI,'flag':flag,'category':category})
+
+#--------------------currency----------------------------
 
 def currency(request):
     flag=False
@@ -17,14 +49,9 @@ def currency(request):
 
     
     if(request.method=='GET'):
-        print('------------------')
-        print(request.GET.get('base_currency'))
-        print(request.GET.get('convert_currency'))
-        print(request.GET.get('amount'))
-        print('------------------')
+
         #curr_form=Currency_form(base_currency=request.GET['base_currency'],convert_currency=request.GET['convert_currency'],amount=request.GET['amount'])
         curr_form=Currency_form(request.GET)
-        print(curr_form)
         #api
         
         if(curr_form.is_valid()):
@@ -38,9 +65,9 @@ def currency(request):
             flag=True
             data=requests.get(url=url)
             data_dict=json.loads(data.text)
-            print('--------')
+            #print('--------')
             conversion_rate=float(data_dict['conversion_rates'][conversion_to])
-            print(conversion_rate)
+            #print(conversion_rate)
             amount=float(amount)
 
             converted_value=amount*conversion_rate
@@ -54,7 +81,63 @@ def currency(request):
     return render(request,'basicapp/currencyconverter.html',{'currency_form':curr_form,'converted_value':converted_value,'flag':flag})
 
 
-
+#---------------------weather---------------------
 
 def weather(request):
-    return render(request,'basicapp/weather.html')
+    flag=False
+
+    city=''
+    country=''
+    temp=''
+    humidity=''
+    wind_speed=''
+    content={'flag':flag,'weather_form':Weather_form() ,'city':city,'country':country,'temp':temp,'humidity':humidity,'wind_speed':wind_speed}
+    if request.method == 'GET':
+
+        weather_form=Weather_form(request.GET)
+
+        if (weather_form.is_valid()):
+        
+            city=request.GET.get('city')
+            state=request.GET.get('state')
+            country=request.GET.get('country')
+
+            #Step 1 get the lattitude and longitude based on city ,state ,country name
+            #http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+            first_url='http://api.openweathermap.org/geo/1.0/direct?q='
+            q=city+','+state+','+country
+            last_part='&limit=1&appid=3afe718612962238f9e5a65484413f37'
+            joined_url=first_url+q+last_part
+            response_lat_long=requests.get(joined_url).json()
+            lat=str(response_lat_long[0]['lat'])
+            lon=str(response_lat_long[0]['lon'])
+
+
+            # STEP 2 getting weather based on lattitude and longitude
+            first_url_wed='https://api.openweathermap.org/data/2.5/weather?'
+            get_wetlatlon='lat='+lat+'&'+'lon='+lon
+            last_url_wed='&appid=3afe718612962238f9e5a65484413f37'
+            joined_url=first_url_wed+get_wetlatlon+last_url_wed
+            response=requests.get(joined_url).json()
+            
+            #result stored in varaiables
+
+            city=response['name']
+            country=response['sys']['country']
+            temp=response['main']['temp']-273.15
+            temp=round(temp,2)
+            humidity=response['main']['humidity']
+            wind_speed=response['wind']['speed']*3.6
+            
+            flag=True
+            content={'flag':flag,'weather_form':weather_form ,'city':city,'country':country,'temp':temp,'humidity':humidity,'wind_speed':wind_speed}
+        else:
+            print(weather_form.errors)
+
+    else:
+        weather_form=Weather_form()
+        
+        
+
+
+    return render(request,'basicapp/weather.html',context=content)
